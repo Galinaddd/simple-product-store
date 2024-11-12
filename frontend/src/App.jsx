23c6404d596242e634226b465d716e3ab2a394ff
Halyna Danylko <danilkogallina@gmail.com>
2024-11-12 13:59:03 +0200

    finished backend and started fronend
    finished backend part and started frontend part: dependencies , vite,  chakra ui

---
+++ frontend/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react';
+import reactLogo from './assets/react.svg';
+import viteLogo from '/vite.svg';
+import { Button, ButtonGroup } from '@chakra-ui/react';
+
+function App() {
+  return (
+    <>
+      <Button>hello</Button>
+    </>
+  );
+}
+
+export default App;
